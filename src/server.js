693623ce046afdaea89668530b1c382d97702e34
Peter Gray <peter@petergraycreative.com>
2018-03-14 23:43:35 -0500

    Finish get at /posts and /top-answer/:soID
    Top Answer suggests using two requests, but that is not necessary. The soID is the same for the parent question and the answer question.
    
    You only need one .find request for parentID and then you can sort and limit to one document.
    
    User Model is not working.

--- src/server.js
+++ src/server.js
@@ -15,11 +15,49 @@ server.use(bodyParser.json());
 server.get('/posts', (req, res) => {
   Post.find({})
     .then((posts) => {
-      return res.status(200).json(posts)
+      return res.status(200).json(posts);
     })
     .catch((err) => {
-      res.status(400).json({ err: err});
-    }); 
+      res.status(400).json({ err: err });
+    });
+});
+
+server.get('/accepted-answer/:soID', (req, res) => {
+  if (req.params.soID) {
+    Post.findOne({ soID: req.params.soID })
+      .then((post) => post.acceptedAnswerID)
+      .then((answerID) => {
+        if (answerID) {
+          Post.findOne({ soID: answerID })
+            .then((answerPost) => {
+              if (answerPost) {
+                res.status(200).json(answerPost);
+              } else res.status(404).json({ error: 'No answer post found.' });
+            })
+            .catch((err) => {
+              return {error: err};
+            });
+        } else
+          res.status(404).json({ error: 'Post does not have an answer ID' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ err: 'Server Error' });
+      });
+  }
+});
+
+server.get('/top-answer/:soID', (req, res) => {
+  if (req.params.soID) {
+    Post.find({ parentID: req.params.soID })
+      .sort({ score: -1 }).limit(1)
+      .then((post) => {
+        if (post.length) {
+        res.status(200).json(post);
+        } else res.status(500).json({err: 'Parent ID not found'});
+      })
+      .catch(err => res.status(500).json({err: err.message}));
+  }
 });
 
 module.exports = { server };
